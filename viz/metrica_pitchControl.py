b0b386a74edefd78aabc7fb982f411e368ff7b2f
xecarlox94 <jf94.uk@gmail.com>
2020-07-21 17:11:28 +0100

    creating pitch control model

--- viz/metrica_pitchControl.py
+++ viz/metrica_pitchControl.py
@@ -1,6 +1,33 @@
 import numpy as np
 
 
+
+class Player(object):
+    def __init__(self, pid, team, teamname, params, GKid):
+        self.id = pid
+        self.isGK = self.id == GKid
+        self.teamname = teamname
+        self.playername = "%s_%s_" % (teamname, self.id)
+        self.vmax = params['max_player_speed']
+        self.reaction_time = params['reaction_time']
+        self.tti_sigma = params['tti_sigma']
+        self.lambda_att = params['lambda_att']
+        self.lambda_def = params['lambda_gk'] if self.isGK else params['lambda_def']
+        self.get_position(team)
+        self.get_velocity(team)
+        self.PPCF = 0
+
+    def get_position(self, team):
+        self.position = np.array( [team[self.playername + 'x'], team[self.playername + 'y']] )
+        self.inframe = not np.any( np.isnan(self.position) )
+
+
+    def get_velocity(self, team):
+        self.velocity = np.array( [team[self.playername + 'vx'], team[self.playername + 'vy']] )
+        if np.any( np.isnan(self.velocity) ):
+            self.velocity = np.array([0., 0.])
+
+
 def default_model_params(time_to_control_veto=3):
     params = {}
     params['max_player_accel'] = 7.
