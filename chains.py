f848c9143715ccc6184b40993d209f51cb4e0d6c
xecarlox94 <jf94.uk@gmail.com>
2020-08-04 23:30:01 +0100

    working on posession chains

--- chains.py
+++ chains.py
@@ -3,13 +3,43 @@ import pandas as pd
 import numpy as np
 
 #competition 9 -> inter - bayern
-competition = sio.getCompetitions()[25]
+competition = sio.getCompetitions()[26]
 
 
-match = sio.getMatches(competition).iloc[6]
+match = sio.getMatches(competition).iloc[17]
 
+
+team_ids = [match['home_team_home_team_id'],match['away_team_away_team_id']]
 
 events = sio.getMatchEvents(match)
 
+limit = 120
 
-passes = events[events['type_id'] == 30]
+
+poss_chains = []
+curr_team_poss = 0
+curr_chain_poss = []
+
+for i, e in events.iterrows():
+    type_id = e['type_id']
+    
+    if type_id in [35, 18]: 
+        continue 
+    
+    if type_id == 30:
+        pass_type = e['pass_type_id']
+        
+        if np.isnan(pass_type):
+            print('continuing chain')
+        
+        if pass_type in [61, 62, 63, 65, 67]:
+            print('-> start chain')
+        
+    elif type_id == 42:
+        print('ball receipt')
+        
+    elif type_id == 43:
+        print('carry')
+    
+    
+    if i > limit: break
