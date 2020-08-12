9936ca2d21a4a9c1a933798a4df7d53e410a6d2b
xecarlox94 <jf94.uk@gmail.com>
2020-08-13 00:03:26 +0100

    rewriting posession chain code

--- chains.py
+++ chains.py
@@ -1,6 +1,5 @@
 import statsBomb_IO as sio
 import pandas as pd
-import numpy as np
 
 
 competition = sio.getCompetitions()[25]
@@ -12,9 +11,73 @@ team_ids = [match['home_team_home_team_id'],match['away_team_away_team_id']]
 events = sio.getMatchEvents(match)
 
 
+class Possession:
+    def __init__(self, home_team_id, away_team_id):
+        self.teams_ids = (home_team_id, away_team_id)
+        self.poss_chains = []
+        self.p_team = -1
+        self.start = -1
+        self.end = -1
+        self.poss_session = False
 
-poss_chains = []
-p = dict()
+    def get_opposition_team_id(self, team_id):
+        if self.teams_ids[0] == team_id:
+            return self.teams_ids[1]
+        elif self.teams_ids[1] == team_id:
+            return self.teams_ids[0]
+        else:
+            assert False, "Wrong team_id (%d)".format(team_id)
+
+    def has_chain_started(self):
+        return self.start != -1 and self.p_team != -1
+
+    def is_same_team_poss(self, p_team):
+        return self.p_team == p_team
+
+    def end_chain(self, end, p_team):
+        if self.has_chain_started():
+            assert self.start <= end, "Invalid end (%d) because it is smaller than start (%d)".format(end, self.start)
+            self.end = end
+            self.append_poss_chain()
+        else:
+            pre_poss = self.poss_chains[len(self.poss_chains) - 1]
+            new_start = pre_poss['start'] + 1
+            self.start_chain(new_start, p_team)
+            self.add_end(end, p_team)
+
+    def start_chain_if_change(self, event_number, p_team, poss_session=False):
+        if self.has_chain_started() and not self.is_same_team_poss(p_team):
+            self.start_chain(event_number, p_team, poss_session)
+
+    def start_chain(self, start, p_team, poss_session=False):
+        if self.has_chain_started():
+            assert self.start < start, "New chain start (%d) is illegal (previous one %d)  ".format(start, self.start)
+            self.end_chain(start - 1, self.p_team)
+        self.start = start
+        self.p_team = p_team
+        self.poss_session = poss_session
+
+    def append_single_event_chain(self, event_number, p_team, poss_session=False):
+        if self.has_chain_started():
+            self.end_chain(event_number - 1)
+        self.start_chain(event_number, p_team, poss_session)
+        self.end_chain(event_number)
+
+    def append_poss_chain(self):
+        assert self.start > self.end, "Possession start (%d) is bigger than its end (%d)".format(self.start, self.end)
+        self.poss_chains.append(
+            {"start": self.start, "end": self.end, "poss_team": self.p_team, "poss_session": self.poss_session}
+        )
+        self.p_team = -1
+        self.start = -1
+        self.end = -1
+        self.poss_session = False
+
+    def dataframe(self):
+        return pd.DataFrame(self.poss_chains)
+
+
+possession = Possession(team_ids[0], team_ids[1])
 skip = -1
 
 for i, e in events.iterrows():
@@ -28,34 +91,17 @@ for i, e in events.iterrows():
         continue
     
     if type_id == 34: # half end
-        #if 'p_team' not in p:
-            #p['p_team'] = team_id
-        
-        if 'str' not in p:
+        if not possession.has_chain_started():
             continue
-        
-        # assert if the start field does not exist
-        p['end'] = i - 1
-        poss_chains.append(p)
-        p = dict()
+        possession.end_chain(i - 1)
     
     if type_id == 9:
-        if 'str' in p:
-            p['end'] = i - 1
-            poss_chains.append(p)
-            
-        p = dict()            
-        p['str'] = i
-        p['p_team'] = team_id
-        p['end'] = i
-        poss_chains.append(p)
-        p = dict()
-        
+        possession.append_poss_chain(i,team_id)
 
-    if type_id == 23: # goalkeepereve
+    if type_id == 23: # goalkeeper
         gk_evt_type = int(e['goalkeeper_type_id'])
         #print(i, gk_evt_type, e['goalkeeper_type_name'])
-        
+
         if gk_evt_type == 25:
             if 'str' in p:
                 p['end'] = i - 1
@@ -131,7 +177,7 @@ for i, e in events.iterrows():
                 p = dict()
                 p['p_team'] = team_id
                 p['str'] = i
-            
+
             if 'p_team' not in p:
                 p['p_team'] = team_id
                 p['str'] = i
@@ -174,9 +220,11 @@ for i, e in events.iterrows():
     
     #if i > limit: break
 
-poss_chains = pd.DataFrame(poss_chains)
+df_poss = possession.dataframe()
 
-poss_chains['diff'] = pd.Series([poss_chains.iloc[i + 1]['str'] - poss_chains.iloc[i]['end'] - 1 for i in range(len(poss_chains) - 1)])
+df_poss['diff'] = pd.Series([df_poss.iloc[i + 1]['start'] - df_poss.iloc[i]['end'] - 1 for i in range(len(df_poss) - 1)])
 
-poss_chains['single_event_chain'] = poss_chains['str'] == poss_chains['end']
+df_poss['single_event_chain'] = df_poss['start'] == df_poss['end']
+
+df_poss['mistake'] = df_poss['start'] > df_poss['end']
 
